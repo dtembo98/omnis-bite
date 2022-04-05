473bd5eae8659bb2deb84563dd867836be2e2dd8
dtembo965 <davidtembo51@gmail.com>
2022-04-05 18:33:11 +0200

    add app icons

---
+++ assets/icons/index.ts
@@ -0,0 +1,16 @@
+export const icons = {
+  facebook: require("./facebook.png"),
+  twitter: require("./twitter.png"),
+  google: require("./google.png"),
+  hide: require("./hide.png"),
+  lock: require("./lock.png"),
+  map_pin: require("./map-pin.png"),
+  message: require("./message.png"),
+  shopping_cart: require("./shopping-cart.png"),
+  user: require("./user.png"),
+  call: require("./call.png"),
+  chevronLeft: require("./chevron-left.png"),
+  email: require("./email.png"),
+}
+
+export type IconTypes = keyof typeof icons
